62b51ccca85753866a63714d21970387b67d071b
Andrey Fedchuk <andrew414414@gmail.com>
2015-12-12 03:34:23 +0300

    .gitignore fixed a bit

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 NGXCFGFOLDER=$(pwd)
 sudo nginx -c $(echo $NGXCFGFOLDER"/nginx.cfg")
 sudo nginx -s reopen
-g++ hello.cpp -lfcgi++ -lfcgi -o videohandler
-spawn-fcgi -p 8000 -n videohandler
+g++ hello.cpp -lfcgi++ -lfcgi -o repzs
+spawn-fcgi -p 8000 -n repzs
